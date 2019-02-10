badd8e27c7b778dfa6638ab18c1f006af3a3902a
Malachi Ivey <zarekbusiness@gmail.com>
2019-02-10 09:17:41 -0500

    text changes

--- JS/scripts.js
+++ JS/scripts.js
@@ -38,43 +38,4 @@ function WidthChange(mq) {
   }
 }
 
-// Variables for The Testimonials Carousel
-const testimonialCarouselContainer = document.querySelectorAll('.client-testimonial-item');
-const carouselSwitch = document.querySelectorAll('.client-testimonial-switch');
-
-// Event Listeners for The Testimonials Carousel
-let carouselCounter = 1;
-showContainer(carouselCounter);
-
-function currentContainer(n) {
-  showContainer(carouselCounter = n);
-}
-
-function showContainer(n) {
-
-  for (let i = 0; i < testimonialCarouselContainer.length; i++) {
-    testimonialCarouselContainer[i].style.display = 'none';
-    testimonialCarouselContainer[i].classList.remove('fast-fade-in-left');
-    carouselSwitch[i].classList.remove('active-carousel-switch');
-  }
-
-  if (carouselCounter > testimonialCarouselContainer.length) {
-    carouselCounter = 1;
-  }
-
-  testimonialCarouselContainer[carouselCounter - 1].style.display = 'block';
-  testimonialCarouselContainer[carouselCounter - 1].classList.add('fast-fade-in-left');
-  carouselSwitch[carouselCounter - 1].classList.add('active-carousel-switch');
-
-  carouselCounter++;
-  setTimeout(showContainer, 20000);
-
-}
-
-carouselSwitch.forEach((button, i) => {
-  button.addEventListener('click', () => {
-    currentContainer(i + 1);
-  });
-});
-
 console.log('If you can see this you\'re awesome!');
